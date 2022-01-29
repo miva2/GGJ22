795b8835ae4a09c87fca61b8ece9718c167a6f6d
Michael Vandendriessche <michael.vandendriessche@oncomfort.com>
2022-01-29 13:29:39 +0100

    Implemented chance to get affix

--- Assets/Scripts/MetalBandNameGenerator.cs
+++ Assets/Scripts/MetalBandNameGenerator.cs
@@ -1,15 +1,20 @@
 using System.Collections;
+using System;
 
 public class MetalBandNameGenerator
 {
     /// chance in a range of 0 to 1. 0 means it will not appear, 1 means it will always appear.
     public static string Generate(string input, float prefixChance, float suffixChance)
     {
-        //TODO: scramble
+        //TODO: scramble (should I still do this?)
         // TODO: apply chance
+        Random random = new Random();
+        float prefixValue = (float)random.NextDouble();
+        float suffixValue = (float)random.NextDouble();
 
-        string prefix = MetalAffix.getPrefixes().GetRandomElement<string>();
-        string suffix = MetalAffix.getSuffixes().GetRandomElement<string>();
+
+        string prefix = prefixChance >= prefixValue ? MetalAffix.getPrefixes().GetRandomElement<string>() : "";
+        string suffix = suffixChance >= suffixValue ? MetalAffix.getSuffixes().GetRandomElement<string>() : "";
 
         return $"{prefix}{input}{suffix}";
     }
